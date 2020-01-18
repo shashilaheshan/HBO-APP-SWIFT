d3fd7b04a7cb455ba09eb8d0b6c24a4b8f62e483
Shashila Heshan <shahshila.heshan@thebetmakers.com>
2020-01-18 10:02:46 +0530

    added face id and touch id

--- HBO/UIControllers/DashboardViewController.swift
+++ HBO/UIControllers/DashboardViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import FirebaseFirestore
 import FirebaseAuth
+import LocalAuthentication
 
 class DashboardViewController: UIViewController {
 
@@ -28,6 +29,85 @@ class DashboardViewController: UIViewController {
        
     }
     
+    @IBAction func btnTouchID(_ sender: Any) {
+        
+         let alert = AlertDialog()
+       
+        
+        let myContext = LAContext()
+        
+        let myLocalizedReasonString = "Biometric Authntication testing !! "
+        
+        var authError: NSError?
+        if #available(iOS 8.0, macOS 10.12.1, *) {
+            if myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+                myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString) { success, evaluateError in
+                    
+                    DispatchQueue.main.async {
+                        if success {
+                            // User authenticated successfully, take appropriate action
+//                            self.successLabel.text = "Awesome!!... User authenticated successfully"
+                            
+                            alert.showAlert(title: "Success", message: "Awesome!!... User authenticated successfully.",buttonText: "Okay")
+                        } else {
+                            // User did not authenticate successfully, look at error and take appropriate action
+                              alert.showAlert(title: "Error", message: "Erro while Authenticating.",buttonText: "Okay")
+                        }
+                    }
+                }
+            } else {
+                // Could not evaluate policy; look at authError and present an appropriate message to user
+               alert.showAlert(title: "Error", message: "Erro while Authenticating.",buttonText: "Okay")
+            }
+        } else {
+            // Fallback on earlier versions
+            
+           alert.showAlert(title: "Error", message: "Erro while Authenticating.",buttonText: "Okay")
+        }
+        
+        
+    }
+
+    @IBAction func btnFaceID(_ sender: Any) {
+        
+        let alert = AlertDialog()
+        
+        let localAuthenticationContext = LAContext()
+        
+        localAuthenticationContext.localizedFallbackTitle = "Please use your Passcode"
+        
+        var authorizationError: NSError?
+        
+        let reason = "Authentication is required for you to continue"
+        
+        if localAuthenticationContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthentication, error: &authorizationError) {
+            
+            _ = localAuthenticationContext.biometryType == LABiometryType.faceID ? "Face ID" : "Touch ID"
+            
+            
+            localAuthenticationContext.evaluatePolicy(LAPolicy.deviceOwnerAuthentication, localizedReason: reason) { (success, evaluationError) in
+                if success {
+                    
+                     alert.showAlert(title: "Success", message: "Awesome!!... User authenticated successfully.",buttonText: "Okay")
+                    
+                } else {
+                    alert.showAlert(title: "Error", message: "Error occured",buttonText: "Okay")
+                    
+                    if let errorObj = evaluationError {
+                        let messageToDisplay = self.getErrorDescription(errorCode: errorObj._code)
+                        
+                         alert.showAlert(title: "Error", message: messageToDisplay,buttonText: "Okay")
+                        
+                    }
+                }
+            }
+            
+        } else {
+            
+             alert.showAlert(title: "Error", message: "User has not enrolled into using Biometricsd",buttonText: "Okay")
+            
+        }
+    }
     @IBAction func btnLogout(_ sender: Any) {
         do {
             try Auth.auth().signOut()
@@ -117,6 +197,40 @@ class DashboardViewController: UIViewController {
         txtDirector.text = ""
         
         txtRating.text = ""
+    }
+    
+    func getErrorDescription(errorCode: Int) -> String {
+        
+        switch errorCode {
+            
+        case LAError.authenticationFailed.rawValue:
+            return "Authentication was not successful, because user failed to provide valid credentials."
+            
+        case LAError.appCancel.rawValue:
+            return "Authentication was canceled by application (e.g. invalidate was called while authentication was in progress)."
+            
+        case LAError.invalidContext.rawValue:
+            return "LAContext passed to this call has been previously invalidated."
+            
+        case LAError.notInteractive.rawValue:
+            return "Authentication failed, because it would require showing UI which has been forbidden by using interactionNotAllowed property."
+            
+        case LAError.passcodeNotSet.rawValue:
+            return "Authentication could not start, because passcode is not set on the device."
+            
+        case LAError.systemCancel.rawValue:
+            return "Authentication was canceled by system (e.g. another application went to foreground)."
+            
+        case LAError.userCancel.rawValue:
+            return "Authentication was canceled by user (e.g. tapped Cancel button)."
+            
+        case LAError.userFallback.rawValue:
+            return "Authentication was canceled, because the user tapped the fallback button (Enter Password)."
+            
+        default:
+            return "Error code \(errorCode) not found"
+        }
+        
     }
 
 }
